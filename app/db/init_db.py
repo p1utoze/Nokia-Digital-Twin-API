bfb03d87c60787521e7fb55ae9c07cf9c36e0c0c
p1utoze <1ds21ai001@dsce.edu.in>
2024-05-23 19:41:10 +0530

    update: modify first user creation fields

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -51,5 +51,6 @@ def init_db(db: Session) -> None:
         user_in = schemas.UserCreate(
             email=settings.FIRST_SUPERUSER,
             password=settings.FIRST_SUPERUSER_PASSWORD,
+            country="USA",
         )
         user = crud.user.create(db, obj_in=user_in, role=Role.ADMIN)  # noqa: F841
